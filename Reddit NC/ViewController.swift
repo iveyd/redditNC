4d40e7de2fcff97376780f87055dd1368244450c
John Behnke <behnkej@me.com>
2015-06-19 18:03:10 -0400

    Initial Commit

---
+++ Reddit NC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reddit NC
+//
+//  Created by John Behnke on 6/19/15.
+//  Copyright (c) 2015 John Behnke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
